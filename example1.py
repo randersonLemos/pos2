04abbc29d594af3c84b8e29d020aa3b597e8e9b8
Randerson Lemos <randerson.lemos@gmail.com>
2019-11-27 09:39:32 -0300

    Fixed paths with sims_folder and sim_folder.

--- example1.py
+++ example1.py
@@ -15,6 +15,11 @@ if __name__ == '__main__':
 
     from config.scripts import settings as sett
 
-    sim_folder = 'sim_016'
-    path_to_rep_file = sett.REP_ROOT / sim_folder / sett.REP_NAME
+    sims_folder = 'SIM_ICV_2_STGS'
+    sim_folder  = 'sim_000'
+    path_to_rep_file = sett.REP_ROOT / sims_folder / sim_folder / sett.REP_NAME
     ref = utils.get_tables(path_to_rep_file)
+
+    from inputt.scripts import resume
+    for well in resume.inje_lst:
+       ref.add(ref.join(well.name, *well.nickname_lst))
