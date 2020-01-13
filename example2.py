f6bd11e8f970bdafbbab4b4a9712863829a3eaa3
Randerson <randerson.lemos@gmail.com>
2020-01-13 16:42:54 -0300

    example2.py working for several directories.

--- example2.py
+++ example2.py
@@ -6,40 +6,33 @@ Created on Thu Aug 22 13:49:04 2019
 """
 
 
+import os
+import shutil
+
 if __name__ == '__main__':
     from os import sys, path
     sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
     import pathlib
-    from scripts import utils
-
+    from post_process.scripts import utils
     from config.scripts import settings as sett
 
-    sim_group_folder = 'REFERENCE'
-    sim_folder  = 'sim_001'
-    path_to_rep_file = sett.REP_ROOT / sim_group_folder / sim_folder / sett.REP_NAME
-    ref = utils.get_tables(path_to_rep_file)
+    sim_group_folders = os.listdir(sett.REP_ROOT / sett.SIMS_FOLDER)
+    for sim_group_folder in sim_group_folders:
+        sim_folders = []
+        for content in os.listdir(sett.REP_ROOT / sett.SIMS_FOLDER / sim_group_folder):
+            if os.path.isdir(sett.REP_ROOT / sett.SIMS_FOLDER / sim_group_folder / content):
+                sim_folders.append(content)
+            elif '.eofcs.csv' in content:
+                shutil.copyfile(sett.REP_ROOT / sett.SIMS_FOLDER / sim_group_folder / content,
+                                   sett.CSV_ROOT / sett.SIMS_FOLDER / sim_group_folder / content)
+        for sim_folder in sim_folders:
+            path_to_rep_file = sett.REP_ROOT / sett.SIMS_FOLDER / sim_group_folder / sim_folder / sett.REP_NAME
+            ref = utils.get_tables(path_to_rep_file)
 
-    from inputt.scripts import resume
-    for well in resume.inje_lst:
-       ref.add(ref.join(well.name, *well.nickname_lst))
+            from inputt import loader
+            for well in loader.inje_lst:
+               ref.add(ref.join(well.name, *well.alias_lst))
 
-    from post_process.scripts import sector_graph
-    sGraph = sector_graph.Sector_Graph(ref)
-
-    from inputt.scripts.resume import prod_lst
-    from post_process.scripts import producer_graph
-    lst = []
-    for well in prod_lst:
-        lst.append(well.name)
-    pGraph = producer_graph.Producer_Graph(lst, ref)
-
-    from post_process.scripts import special_graph
-    spGraph = special_graph.Special_Graph(lst, ref)
-
-    from inputt.scripts.resume import inje_lst
-    from post_process.scripts import injector_graph
-    lst = []
-    for well in inje_lst:
-        lst.append(well.name)
-    iGraph = injector_graph.Injector_Graph(lst, ref)
+            output_dir = sett.CSV_ROOT / sett.SIMS_FOLDER/ sim_group_folder / sim_folder
+            ref.to_csv(output_dir)
